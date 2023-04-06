8b5d2acc78ad6fae34b59824dc9a8e3451999a6f
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2023-04-06 20:00:38 -0300

    Created headers to fix the CORS error

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_APIURL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  },
 });
 
 export default api;
