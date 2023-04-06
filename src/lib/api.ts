5799d0b52ace14d2520d81d54c639884504b3210
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2023-04-06 20:05:38 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,8 +4,9 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_APIURL,
   headers: {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Content-Type": "application/json",
   },
 });
 
